a473e1cf670e6558acb2a3adde41c2cfc07a9d95
White, Kevin (ETW - FLEX) <Kevin.White@nike.com>
2018-07-15 17:13:13 -0700

    basic unit test scripts

---
+++ scripts/test/unit.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
+CURRENT_DIR="${CURRENT_DIR:?}"
+SCRIPTS_DIR="$(realpath "${CURRENT_DIR}/..")"
+ROOT_DIR="$(realpath "${SCRIPTS_DIR}/..")"
+TEST_DIR="${ROOT_DIR}/test/unit"
+REPORTS_DIR="${ROOT_DIR}/reports/unit"
+
+yarn mocha \
+  --recursive "${TEST_DIR}/**/*.js" \
+  --reporter-options reportDir="${REPORTS_DIR}/unit" 
