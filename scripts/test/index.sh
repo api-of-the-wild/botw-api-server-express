a1410be82858fe48cdc07807d99da6eafe3efc7b
White, Kevin (ETW - FLEX) <Kevin.White@nike.com>
2018-08-05 20:28:13 -0700

    added aggregate test script

---
+++ scripts/test/index.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Get directory path of *this* script file and exit if is not set, NULL, or an empty string
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
+CURRENT_DIR="${CURRENT_DIR:?}"
+SCRIPTS_DIR="$(realpath "${CURRENT_DIR}/..")"
+ROOT_DIR="$(realpath "${SCRIPTS_DIR}/..")"
+# TEST_DIR="${ROOT_DIR}/test/unit"
+# REPORTS_DIR="${ROOT_DIR}/reports/unit"
+# source the build.lib functions
+# . "${SCRIPTS_DIR}/lib.sh"
+
+set -x
+
+yarn test:lint
+yarn test:unit
+yarn test:coverage
