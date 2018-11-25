2f57fa64d425e2b6d71fec8246e591151103847e
Kevin White <kwhitejr@gmail.com>
2018-11-25 11:49:34 -0800

    script for arrows and shields

---
+++ scripts/db/compendium.sql
@@ -0,0 +1,73 @@
+-- GRANT ALL PRIVILEGES ON DATABASE botw TO admin;
+
+-- DROP MATERIALIZED VIEW IF EXISTS location_views;
+DROP TABLE IF EXISTS weapons;
+DROP TABLE IF EXISTS bows;
+DROP TABLE IF EXISTS arrows;
+DROP TABLE IF EXISTS shields;
+
+CREATE TABLE weapons
+(
+  id SERIAL PRIMARY KEY,
+  compendium_id INT NULL,
+  compendium_id_dlc_2 INT NULL,
+  compendium_id_master_mode INT NULL,
+  compendium_id_master_mode_dlc_2 INT NULL,
+  name TEXT NOT NULL,
+  hands INT,
+  weapon_type TEXT,
+  attack_power INT NULL,
+  durability INT NULL,
+  locations INT[] NULL,
+  dropped_by TEXT NULL,
+  description TEXT
+);
+
+-- CREATE TABLE bows
+-- (
+--   id INT PRIMARY KEY NOT NULL,
+--   compendium_id INT,
+--   compendium_id_dlc_2 INT,
+--   compendium_id_master_mode INT,
+--   compendium_id_master_mode_dlc_2 INT,
+--   name TEXT NOT NULL,
+--   hands INT,
+--   weapon_type TEXT,
+--   attack_power INT,
+--   durability INT,
+--   description TEXT
+-- );
+
+CREATE TABLE arrows
+(
+  id INT PRIMARY KEY NOT NULL,
+  compendium_id INT NULL,
+  compendium_id_dlc_2 INT NULL,
+  compendium_id_master_mode INT NULL,
+  compendium_id_master_mode_dlc_2 INT NULL,
+  name TEXT NOT NULL,
+  attack_power_min INT,
+  attack_power_max INT,
+  effects TEXT,
+  description TEXT
+);
+
+CREATE TABLE shields
+(
+  id INT PRIMARY KEY NOT NULL,
+  compendium_id INT NULL,
+  compendium_id_dlc_2 INT NULL,
+  compendium_id_master_mode INT NULL,
+  compendium_id_master_mode_dlc_2 INT NULL,
+  name TEXT NOT NULL,
+  strength INT,
+  durability INT,
+  locations INT[] NULL,
+  dropped_by TEXT NULL,
+  description TEXT
+);
+
+COPY weapons FROM '/Users/kwhitejr/Projects/api-of-the-wild/botw-api-server-express/db/data/compendium/weapons.csv' DELIMITER ',' CSV HEADER;
+-- COPY bows FROM '/Users/kwhitejr/Projects/api-of-the-wild/botw-api-server-express/db/data/compendium/bows.csv' DELIMITER ',' CSV HEADER;
+COPY arrows FROM '/Users/kwhitejr/Projects/api-of-the-wild/botw-api-server-express/db/data/compendium/arrows.csv' DELIMITER ',' CSV HEADER;
+COPY shields FROM '/Users/kwhitejr/Projects/api-of-the-wild/botw-api-server-express/db/data/compendium/shields.csv' DELIMITER ',' CSV HEADER;
---
+++ src/routes/compendium.js
@@ -0,0 +1,118 @@
+/* eslint-disable no-unused-vars*/
+const router = require("express").Router();
+
+const {
+  asyncMiddleware,
+  enrichResponseMiddleware,
+} = require("../utilities/middleware");
+
+const {
+  getLocation,
+  getSubregion,
+  getRegion,
+} = require("../queries/compendium");
+
+const routes = app => {
+  const db = app.get("db");
+
+  router.get(
+    "/materials/v1/:id",
+    asyncMiddleware(async (req, res, next) => {
+      const id = req.params.id;
+      // const location = await getLocation(db, id);
+      // if (location === null) {
+      //   res.status(404).send({
+      //     message: `Locations resource with id ${id} does not exist.`,
+      //   });
+      //   return;
+      // }
+      // res.body = location;
+      next();
+    })
+  );
+
+  router.get(
+    "/weapons/v1/:id",
+    asyncMiddleware(async (req, res, next) => {
+      const id = req.params.id;
+      // const subregion = await getSubregion(db, id);
+      // if (subregion === null) {
+      //   res.status(404).send({
+      //     message: `Subregions resource with id ${id} does not exist.`,
+      //   });
+      //   return;
+      // }
+      // res.body = subregion;
+      next();
+    })
+  );
+
+  router.get(
+    "/bows/v1/:id",
+    asyncMiddleware(async (req, res, next) => {
+      const id = req.params.id;
+      // const region = await getRegion(db, id);
+      // if (region === null) {
+      //   res.status(404).send({
+      //     message: `Regions resource with id ${id} does not exist.`,
+      //   });
+      //   return;
+      // }
+      // res.body = region;
+      next();
+    })
+  );
+
+  router.get(
+    "/arrows/v1/:id",
+    asyncMiddleware(async (req, res, next) => {
+      const id = req.params.id;
+      // const region = await getRegion(db, id);
+      // if (region === null) {
+      //   res.status(404).send({
+      //     message: `Regions resource with id ${id} does not exist.`,
+      //   });
+      //   return;
+      // }
+      // res.body = region;
+      next();
+    })
+  );
+
+  router.get(
+    "/shields/v1/:id",
+    asyncMiddleware(async (req, res, next) => {
+      const id = req.params.id;
+      // const region = await getRegion(db, id);
+      // if (region === null) {
+      //   res.status(404).send({
+      //     message: `Regions resource with id ${id} does not exist.`,
+      //   });
+      //   return;
+      // }
+      // res.body = region;
+      next();
+    })
+  );
+
+  router.get(
+    "/treasure/v1/:id",
+    asyncMiddleware(async (req, res, next) => {
+      const id = req.params.id;
+      // const region = await getRegion(db, id);
+      // if (region === null) {
+      //   res.status(404).send({
+      //     message: `Regions resource with id ${id} does not exist.`,
+      //   });
+      //   return;
+      // }
+      // res.body = region;
+      next();
+    })
+  );
+
+  router.use(enrichResponseMiddleware);
+  return router;
+};
+
+module.exports = routes;
