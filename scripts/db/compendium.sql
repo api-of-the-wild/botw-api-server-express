69397cab6f9079e3b0d28a71df44a1b54097cd4e
Kevin White <kwhitejr@gmail.com>
2018-12-08 17:10:53 -0800

    improved script for seeding compendium

--- scripts/db/compendium.sql
+++ scripts/db/compendium.sql
@@ -8,7 +8,7 @@ DROP TABLE IF EXISTS shields;
 
 CREATE TABLE weapons
 (
-  id SERIAL PRIMARY KEY,
+  id INT PRIMARY KEY NOT NULL,
   compendium_id INT NULL,
   compendium_id_dlc_2 INT NULL,
   compendium_id_master_mode INT NULL,
@@ -23,20 +23,22 @@ CREATE TABLE weapons
   description TEXT
 );
 
--- CREATE TABLE bows
--- (
---   id INT PRIMARY KEY NOT NULL,
---   compendium_id INT,
---   compendium_id_dlc_2 INT,
---   compendium_id_master_mode INT,
---   compendium_id_master_mode_dlc_2 INT,
---   name TEXT NOT NULL,
---   hands INT,
---   weapon_type TEXT,
---   attack_power INT,
---   durability INT,
---   description TEXT
--- );
+CREATE TABLE bows
+(
+  id INT PRIMARY KEY NOT NULL,
+  compendium_id INT,
+  compendium_id_dlc_2 INT,
+  compendium_id_master_mode INT,
+  compendium_id_master_mode_dlc_2 INT,
+  name TEXT NOT NULL,
+  attack_power INT,
+  durability INT,
+  range INT,
+  fire_rate INT,
+  locations INT[] NULL,
+  dropped_by TEXT NULL,
+  description TEXT
+);
 
 CREATE TABLE arrows
 (
@@ -68,6 +70,6 @@ CREATE TABLE shields
 );
 
 COPY weapons FROM '/Users/kwhitejr/Projects/api-of-the-wild/botw-api-server-express/db/data/compendium/weapons.csv' DELIMITER ',' CSV HEADER;
--- COPY bows FROM '/Users/kwhitejr/Projects/api-of-the-wild/botw-api-server-express/db/data/compendium/bows.csv' DELIMITER ',' CSV HEADER;
+COPY bows FROM '/Users/kwhitejr/Projects/api-of-the-wild/botw-api-server-express/db/data/compendium/bows.csv' DELIMITER ',' CSV HEADER;
 COPY arrows FROM '/Users/kwhitejr/Projects/api-of-the-wild/botw-api-server-express/db/data/compendium/arrows.csv' DELIMITER ',' CSV HEADER;
 COPY shields FROM '/Users/kwhitejr/Projects/api-of-the-wild/botw-api-server-express/db/data/compendium/shields.csv' DELIMITER ',' CSV HEADER;
