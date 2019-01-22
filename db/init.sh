5b0d08eb7825ddd47fbdc2e964de7d63a72b201b
Kevin White <kwhitejr@gmail.com>
2019-01-21 21:29:45 -0800

    updated postgres container init script

--- db/init.sh
+++ db/init.sh
@@ -4,72 +4,70 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" "$POSTGRES_DB" <<-EOSQL
     GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;
 
-    -- Init /geography domain
-    DROP MATERIALIZED VIEW IF EXISTS location_views;
-    DROP TABLE IF EXISTS locations;
-    DROP TABLE IF EXISTS location_types;
-    DROP TABLE IF EXISTS subregions;
-    DROP TABLE IF EXISTS regions;
+    -- Init /crafting domain
+    DROP MATERIALIZED VIEW IF EXISTS creature_views;
+    DROP MATERIALIZED VIEW IF EXISTS monster_views;
 
-    CREATE TABLE location_types
+    DROP TABLE IF EXISTS recoverable_materials;
+
+    CREATE TABLE recoverable_materials
     (
-        id INT PRIMARY KEY NOT NULL,
-        name TEXT NOT NULL
+    id INT PRIMARY KEY NOT NULL,
+    compendium_id INT NULL,
+    compendium_id_dlc_2 INT NULL,
+    compendium_id_master_mode INT NULL,
+    compendium_id_master_mode_dlc_2 INT NULL,
+    name TEXT NOT NULL,
+    material_type TEXT NOT NULL,
+    value INT NULL,
+    restores DECIMAL NULL,
+    additional_uses INT[] NULL,
+    description TEXT NULL
     );
 
-    CREATE TABLE regions
-    (
-        id INT PRIMARY KEY NOT NULL,
-        name TEXT NOT NULL
-    );
-
-    CREATE TABLE subregions
-    (
-        id INT PRIMARY KEY NOT NULL,
-        name TEXT NOT NULL,
-        region_fk INT references regions(id)
-    );
-
-    CREATE TABLE locations
-    (
-        id INT PRIMARY KEY NOT NULL,
-        name TEXT NOT NULL,
-        region_fk INT references regions(id),
-        subregion_fk INT references subregions(id),
-        location_type_fk INT references location_types(id)
-    );
-
-    COPY location_types FROM '/docker-entrypoint-initdb.d/data/geography/locationType.csv' DELIMITER ',' CSV HEADER;
-    COPY regions FROM '/docker-entrypoint-initdb.d/data/geography/region.csv' DELIMITER ',' CSV HEADER;
-    COPY subregions FROM '/docker-entrypoint-initdb.d/data/geography/subregion.csv' DELIMITER ',' CSV HEADER;
-    COPY locations FROM '/docker-entrypoint-initdb.d/data/geography/location.csv' DELIMITER ',' CSV HEADER;
-
-    CREATE MATERIALIZED VIEW location_views
-    AS
-    SELECT
-        R.id AS region_id,
-        R.name AS region,
-        S.id AS subregion_id,
-        S.name AS subregion,
-        L.id AS location_id,
-        L.name AS location,
-        T.id AS location_type_id,
-        T.name AS location_type
-        FROM locations L
-        LEFT JOIN regions R ON R.id = L.region_fk 
-        LEFT JOIN subregions S ON S.id = L.subregion_fk
-        LEFT JOIN location_types T ON T.id = L.location_type_fk
-    ORDER BY L.id ASC
-    WITH DATA;
+    COPY recoverable_materials FROM '/docker-entrypoint-initdb.d/data/compendium/recoverable_materials.csv' DELIMITER ',' CSV HEADER;
 
     -- Init /compendium domain
     DROP MATERIALIZED VIEW IF EXISTS material_views;
+    DROP MATERIALIZED VIEW IF EXISTS monster_views;
+    DROP MATERIALIZED VIEW IF EXISTS creature_views;
+
+    DROP TABLE IF EXISTS creatures;
+    DROP TABLE IF EXISTS monsters;
     DROP TABLE IF EXISTS materials_additional_uses;
     DROP TABLE IF EXISTS materials;
     DROP TABLE IF EXISTS weapons;
     DROP TABLE IF EXISTS bows;
     DROP TABLE IF EXISTS arrows;
     DROP TABLE IF EXISTS shields;
+
+    CREATE TABLE creatures
+    (
+        id INT PRIMARY KEY NOT NULL,
+        compendium_id INT NULL,
+        compendium_id_dlc_2 INT NULL,
+        compendium_id_master_mode INT NULL,
+        compendium_id_master_mode_dlc_2 INT NULL,
+        name TEXT NOT NULL,
+        creature_type TEXT NOT NULL,
+        recoverable_materials INT[] NULL,
+        description TEXT NOT NULL
+    );
+
+    CREATE TABLE monsters
+    (
+        id INT PRIMARY KEY NOT NULL,
+        compendium_id INT NULL,
+        compendium_id_dlc_2 INT NULL,
+        compendium_id_master_mode INT NULL,
+        compendium_id_master_mode_dlc_2 INT NULL,
+        name TEXT NOT NULL,
+        monster_type TEXT NOT NULL,
+        health INT NULL,
+        base_damage INT NULL,
+        recoverable_materials INT[] NULL,
+        description TEXT NOT NULL
+    );
 
     CREATE TABLE materials_additional_uses
     (
@@ -163,6 +161,42 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" "$POSTGRES_DB" <<-EOSQL
     COPY shields FROM '/docker-entrypoint-initdb.d/data/compendium/shields.csv' DELIMITER ',' CSV HEADER;
 
     -- Data structured for consumer
+    CREATE MATERIALIZED VIEW creature_views
+    AS
+    SELECT
+        creatures.compendium_id,
+        creatures.compendium_id_dlc_2,
+        creatures.compendium_id_master_mode,
+        creatures.compendium_id_master_mode_dlc_2,
+        creatures.name,
+        creatures.creature_type,
+        array_agg(recoverable_materials.name) as recoverable_materials,
+        creatures.description
+    FROM creatures
+    LEFT JOIN recoverable_materials on recoverable_materials.id = any(recoverable_materials)
+    GROUP BY creatures.id
+    ORDER BY creatures.id ASC
+    WITH DATA;
+
+    CREATE MATERIALIZED VIEW monster_views
+    AS
+    SELECT
+        monsters.compendium_id,
+        monsters.compendium_id_dlc_2,
+        monsters.compendium_id_master_mode,
+        monsters.compendium_id_master_mode_dlc_2,
+        monsters.name,
+        monsters.monster_type,
+        monsters.health,
+        monsters.base_damage,
+        array_agg(recoverable_materials.name) as recoverable_materials,
+        monsters.description
+    FROM monsters
+    LEFT JOIN recoverable_materials on recoverable_materials.id = any(recoverable_materials)
+    GROUP BY monsters.id
+    ORDER BY monsters.id ASC
+    WITH DATA;
+
     CREATE MATERIALIZED VIEW material_views
     AS
     SELECT
@@ -180,5 +214,63 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" "$POSTGRES_DB" <<-EOSQL
     LEFT JOIN materials_additional_uses on materials_additional_uses.id = any(additional_uses)
     GROUP BY materials.id
     ORDER BY materials.id ASC
+    WITH DATA;
+
+    -- Init /geography domain
+    DROP MATERIALIZED VIEW IF EXISTS location_views;
+    DROP TABLE IF EXISTS locations;
+    DROP TABLE IF EXISTS location_types;
+    DROP TABLE IF EXISTS subregions;
+    DROP TABLE IF EXISTS regions;
+
+    CREATE TABLE location_types
+    (
+        id INT PRIMARY KEY NOT NULL,
+        name TEXT NOT NULL
+    );
+
+    CREATE TABLE regions
+    (
+        id INT PRIMARY KEY NOT NULL,
+        name TEXT NOT NULL
+    );
+
+    CREATE TABLE subregions
+    (
+        id INT PRIMARY KEY NOT NULL,
+        name TEXT NOT NULL,
+        region_fk INT references regions(id)
+    );
+
+    CREATE TABLE locations
+    (
+        id INT PRIMARY KEY NOT NULL,
+        name TEXT NOT NULL,
+        region_fk INT references regions(id),
+        subregion_fk INT references subregions(id),
+        location_type_fk INT references location_types(id)
+    );
+
+    COPY location_types FROM '/docker-entrypoint-initdb.d/data/geography/locationType.csv' DELIMITER ',' CSV HEADER;
+    COPY regions FROM '/docker-entrypoint-initdb.d/data/geography/region.csv' DELIMITER ',' CSV HEADER;
+    COPY subregions FROM '/docker-entrypoint-initdb.d/data/geography/subregion.csv' DELIMITER ',' CSV HEADER;
+    COPY locations FROM '/docker-entrypoint-initdb.d/data/geography/location.csv' DELIMITER ',' CSV HEADER;
+
+    CREATE MATERIALIZED VIEW location_views
+    AS
+    SELECT
+        R.id AS region_id,
+        R.name AS region,
+        S.id AS subregion_id,
+        S.name AS subregion,
+        L.id AS location_id,
+        L.name AS location,
+        T.id AS location_type_id,
+        T.name AS location_type
+        FROM locations L
+        LEFT JOIN regions R ON R.id = L.region_fk 
+        LEFT JOIN subregions S ON S.id = L.subregion_fk
+        LEFT JOIN location_types T ON T.id = L.location_type_fk
+    ORDER BY L.id ASC
     WITH DATA;
 EOSQL
